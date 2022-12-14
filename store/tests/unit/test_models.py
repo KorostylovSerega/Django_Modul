fe8e726fb7046f4ecc4c64a3dac43ae8483978db
KorostylovSerega <armfighter89@gmail.com>
2022-12-14 16:32:18 +0200

    add test directory

---
+++ store/tests/unit/test_models.py
@@ -0,0 +1,17 @@
+from django.test import TestCase
+
+
+class MyUserTestCase(TestCase):
+    pass
+
+
+class ProductTestCase(TestCase):
+    pass
+
+
+class PurchaseTestCase(TestCase):
+    pass
+
+
+class ReturnPurchaseTestCase(TestCase):
+    pass
