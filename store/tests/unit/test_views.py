934549224efa7a4adee9d2a8e906e42d98f00dcb
KorostylovSerega <armfighter89@gmail.com>
2022-12-19 16:46:07 +0200

    add test

--- store/tests/unit/test_views.py
+++ store/tests/unit/test_views.py
@@ -1,27 +1,34 @@
 from django.contrib.auth import authenticate, login
-from django.test import TestCase, RequestFactory
+from django.contrib.auth.middleware import AuthenticationMiddleware
+from django.contrib.auth.models import AnonymousUser
+from django.contrib.sessions.backends.db import SessionStore
+from django.contrib.sessions.middleware import SessionMiddleware
+from django.test import TestCase, RequestFactory, Client
 from django.urls import reverse
 
+from store.forms import UserCreateForm
 from store.models import MyUser
 from store.views import UserCreateView
 
 
 class UserCreateViewTestCase(TestCase):
 
-    def setUp(self):
-        self.form_data = {
-            'username': 'simple',
-            'password': 'simple-password'
-        }
-        MyUser.objects.create_user(username=self.form_data.get('username'),
-                                   password=self.form_data.get('password'))
-
-    def test_form_valid_is_makes_login(self):
-        username = self.form_data.get('username')
-        password = self.form_data.get('password')
-        user = authenticate(username=username,
-                            password=password)
-        self.assertTrue(user.is_authenticated)
+    def test_form_valid(self):
+        form = UserCreateForm(data={'username': 'simple',
+                                    'email': 'simplecustomer@gmail.com',
+                                    'password1': 'simplepassword',
+                                    'password2': 'simplepassword',
+                                    'deposit': 2000})
+        self.assertTrue(form.is_valid())
+        request = RequestFactory().post(reverse('register'))
+        request.session = SessionStore()
+        request.user = AnonymousUser()
+        print(request.user)
+        view = UserCreateView()
+        view.setup(request)
+        view.form_valid(form)
+        print(request.user)
+        self.assertTrue(request.user.is_authenticated)
 
 
 class PurchaseListViewTestCase(TestCase):
